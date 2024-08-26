26b194e8174d12a3e80d1484b2d2d6a8d4a4822f
Cr1staix <sayhell042@gmail.com>
2024-08-26 16:58:13 +0700

    add else if from teaBags

--- src/coffe.java
+++ src/coffe.java
@@ -3,15 +3,17 @@ public class coffe {
         int coffeeAmount = 2330;
         int milkAmount = 3210;
         int skinnedMilkAmount = 1290;
-
+        int teaBags = 3;
         boolean isBlocked = true;
 
         int cappucinoMilkRequired = 60;
         int cappucinoCoffeeRequired = 15;
 
         if (!isBlocked && milkAmount >= cappucinoMilkRequired &&
-            coffeeAmount >= cappucinoCoffeeRequired) {
+                coffeeAmount >= cappucinoCoffeeRequired) {
             System.out.println("Готовим кофе");
+        } else if (teaBags > 0) {
+            System.out.println("Завариваем чай");
         } else {
             System.out.println("Что-то пошло не так ;(");
         }

7df04fa6bb09e41c08557ad9cb4cf6e445015b62
maxdd <maxizi1983@gmail.com>
2024-08-26 17:15:40 +0500

    Initial commit

--- src/coffe.java
+++ src/coffe.java
@@ -1,21 +1,46 @@
-public class coffe {
+import java.util.Scanner;
+
+class CoffeeMachine {
     public static void main(String[] args) {
         int coffeeAmount = 2330;
         int milkAmount = 3210;
         int skinnedMilkAmount = 1290;
         int teaBags = 3;
-        boolean isBlocked = true;
-
-        int cappucinoMilkRequired = 60;
-        int cappucinoCoffeeRequired = 15;
-
-        if (!isBlocked && milkAmount >= cappucinoMilkRequired &&
-                coffeeAmount >= cappucinoCoffeeRequired) {
-            System.out.println("Готовим кофе");
-        } else if (teaBags > 0) {
-            System.out.println("Завариваем чай");
+        boolean isBlocked = true; // Предположим, что машина заблокирована
+        int cappuccinoMilkRequired = 60;
+        int cappuccinoCoffeeRequired = 15;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Выберите напиток (1 - Капучино, 2 - Чай):");
+        int choice = scanner.nextInt();
+        if (!isBlocked) {
+            switch (choice) {
+                case 1:
+                    if (canMakeCappuccino(coffeeAmount, milkAmount, cappuccinoCoffeeRequired, cappuccinoMilkRequired)) {
+                        System.out.println("Готовим кофе");
+                    } else {
+                        System.out.println("Недостаточно ингредиентов для кофе");
+                    }
+                    break;
+                case 2:
+                    if (canMakeTea(teaBags)) {
+                        System.out.println("Завариваем чай");
+                    } else {
+                        System.out.println("Недостаточно чайных пакетиков");
+                    }
+                    break;
+                default:
+                    System.out.println("Неверный выбор");
+                    break;
+            }
         } else {
-            System.out.println("Что-то пошло не так ;(");
+            System.out.println("Машина заблокирована");
         }
+        scanner.close();
+    }
+    public static boolean canMakeCappuccino(int coffeeAmount, int milkAmount, int coffeeRequired, int milkRequired) {
+        return coffeeAmount >= coffeeRequired && milkAmount >= milkRequired;
+    }
+    public static boolean canMakeTea(int teaBags) {
+        return teaBags > 0;
     }
 }
